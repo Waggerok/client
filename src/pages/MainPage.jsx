d202b3a670445618eda27263a758f75a8a6bba80
Waggerok <115732069+Waggerok@users.noreply.github.com>
2024-12-08 20:12:12 +0300

    rewrite deviceSource

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import DeviceList from '../components/DeviceList';
 import axios from 'axios';
 
-const devicesSource = 'http://localhost:5000/api/devices';
+const devicesSource = 'server-production-1e16.up.railway.app/api/devices';
 
 const MainPage = () => {
 
