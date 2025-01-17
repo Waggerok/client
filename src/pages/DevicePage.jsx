a952ad20b7d12a0e74eaf70ef2eb28370a9c03ff
Waggerok <115732069+Waggerok@users.noreply.github.com>
2025-01-17 21:35:20 +0300

    created model.jsx, created .env, added one thing to package.json, added one thing to .gitignore, added condition for button

--- src/pages/DevicePage.jsx
+++ src/pages/DevicePage.jsx
@@ -1,6 +1,8 @@
 import React, {useEffect, useState} from 'react';
 import { useParams } from 'react-router-dom';
 import axios from 'axios';
+import { Canvas } from 'react-three-fiber';
+import { OrbitControls } from '@react-three/drei';
 
 const devicesSource = 'https://server-production-1e16.up.railway.app/api/devices';
 const ImageSource = 'https://server-production-1e16.up.railway.app';
@@ -28,13 +30,33 @@ const DevicePage = () => {
             <div>Loading...</div>
         )
     }
+
+    const turnOn3D = () => {
+        if (!is3D) {
+            setIs3D(true)
+        } else {
+            setIs3D(false)
+        }
+    }
     
     return (
         <div className='App'>
             <div className='device'>
                 <div className="device__items">
                     <div className="device__items_image">
-                        <img src={ImageSource+device.image} alt={device.name} />
+                        {
+                            is3D
+                            ?
+                            <Canvas>
+                                <OrbitControls/>
+                                <mesh>
+                                    <boxGeometry/>
+                                    <meshBasicMaterial color={"red"} />
+                                </mesh>
+                            </Canvas>
+                            :
+                            <img src={ImageSource+device.image} alt={device.name} />
+                        }
                     </div>
                     <div className="device__items_name">
                         <p>{device.name}</p>
@@ -49,7 +71,7 @@ const DevicePage = () => {
             </div>
             <div className="buttons">
                 <div className="buttons__items">
-                    <button className='buttons__items_3d'>3D</button>
+                    <button className='buttons__items_3d' onClick={turnOn3D}>3D</button>
                     <button className='buttons__items_order'>Добавить в корзину</button>
                 </div>
             </div>
