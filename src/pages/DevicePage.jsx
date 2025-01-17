f296e2028fb04cb23fd1339f6d22a2cee16290b7
Waggerok <115732069+Waggerok@users.noreply.github.com>
2025-01-17 12:26:51 +0300

    styled device page

--- src/pages/DevicePage.jsx
+++ src/pages/DevicePage.jsx
@@ -3,10 +3,14 @@ import { useParams } from 'react-router-dom';
 import axios from 'axios';
 
 const devicesSource = 'https://server-production-1e16.up.railway.app/api/devices';
+const ImageSource = 'https://server-production-1e16.up.railway.app';
+
 
 const DevicePage = () => {
     const { id } = useParams();
     const [device,setDevice] = useState(null);
+
+    const [is3D, setIs3D] = useState(false);
 
     useEffect(() => {
         axios
@@ -26,10 +30,29 @@ const DevicePage = () => {
     }
     
     return (
-        <div>
-            <h1>{device.name}</h1>
-            <p>{device.description}</p>
-            <p>Цена: {device.price} руб.</p>
+        <div className='App'>
+            <div className='device'>
+                <div className="device__items">
+                    <div className="device__items_image">
+                        <img src={ImageSource+device.image} alt={device.name} />
+                    </div>
+                    <div className="device__items_name">
+                        <p>{device.name}</p>
+                    </div>
+                    <div className="device__items_description">
+                        Описание: {device.description}
+                    </div>
+                    <div className="device__items_price">
+                        Цена: {device.price} руб.
+                    </div>
+                </div>
+            </div>
+            <div className="buttons">
+                <div className="buttons__items">
+                    <button className='buttons__items_3d'>3D</button>
+                    <button className='buttons__items_order'>Добавить в корзину</button>
+                </div>
+            </div>
         </div>
     );
 };
