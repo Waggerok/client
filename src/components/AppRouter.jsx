5d29ed7d5289576f6cb1cb37d67d8acd1e4919cc
Waggerok <115732069+Waggerok@users.noreply.github.com>
2025-02-03 14:03:15 +0300

    created routes

--- src/components/AppRouter.jsx
+++ src/components/AppRouter.jsx
@@ -9,6 +9,11 @@ import OrdersPage from '../pages/OrdersPage';
 import DevicePage from '../pages/DevicePage';
 import AdminPage from '../pages/AdminPage';
 import Checkout from '../pages/Checkout';
+import AddDevicePage from '../pages/AddDevicePage';
+import EditDevicePage from '../pages/EditDevicePage';
+import DeleteDevicePage from '../pages/DeleteDevicePage';
+import AddAdminPage from '../pages/AddAdminPage';
+import DeleteAdminPage from '../pages/DeleteAdminPage';
 
 const AppRouter = () => {
     return (
@@ -19,6 +24,11 @@ const AppRouter = () => {
             <Route path='device/:id' element={<DevicePage/>}></Route>
             <Route path='checkout' element={<Checkout/>}></Route>
             <Route path='admin' element={<AdminPage/>}></Route>
+            <Route path='admin/add-device' element={<AddDevicePage/>}></Route>
+            <Route path='admin/edit-device' element={<EditDevicePage/>}></Route>
+            <Route path='admin/delete-device' element={<DeleteDevicePage/>}></Route>
+            <Route path='admin/add-admin' element={<AddAdminPage/>}></Route>
+            <Route path='admin/delete-admin' element={<DeleteAdminPage/>}></Route>
             <Route path="*" element={<Navigate to="/" />}></Route>
         </Routes>
     );
--- src/pages/AdminPage.jsx
+++ src/pages/AdminPage.jsx
@@ -1,16 +1,45 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const AdminPage = () => {
+
+    const navigate = useNavigate();
+
     return (
         <div className='App'>
             <h2 style={{ marginTop: '10px', textAlign: 'center' }}>Админ панель</h2>
             <div className="admin">
                 <div className="admin__buttons">
-                    <button style={{ backgroundColor: '#0f5bdd', color: 'white' }}>Добавить устройство</button>
-                    <button style={{ backgroundColor: '#0f5bdd', color: 'white' }}>Редактировать устройство</button>
-                    <button style={{ backgroundColor: '#0f5bdd', color: 'white' }}>Назначить админа</button>
-                    <button style={{ backgroundColor: 'white', color: 'red', border: '1px solid red' }}>Удалить устройство</button>
-                    <button style={{ backgroundColor: 'white', color: 'red', border: '1px solid red' }}>Удалить админа</button>
+                    <button
+                        style={{ backgroundColor: '#0f5bdd', color: 'white' }}
+                        onClick={() => navigate('admin/add-device')}
+                    >
+                        Добавить устройство
+                    </button>
+                    <button
+                        style={{ backgroundColor: '#0f5bdd', color: 'white' }}
+                        onClick={() => navigate('admin/edit-device')}
+                    >
+                        Редактировать устройство
+                    </button>
+                    <button 
+                        style={{ backgroundColor: '#0f5bdd', color: 'white' }}
+                        onClick={() => navigate('admin/add-admin')}
+                    >
+                        Назначить админа
+                    </button>
+                    <button
+                        style={{ backgroundColor: 'white', color: 'red', border: '1px solid red' }}
+                        onClick={() => navigate('admin/delete-device')}
+                    >
+                        Удалить устройство
+                    </button>
+                    <button 
+                        style={{ backgroundColor: 'white', color: 'red', border: '1px solid red' }}
+                        onClick={() => navigate('admin/delete-admin')}
+                    >
+                        Удалить админа
+                    </button>
                 </div>
             </div>
         </div>
