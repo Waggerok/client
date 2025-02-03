27ba86e0486b7fee01a1a4a798368f4b39f34d55
Waggerok <115732069+Waggerok@users.noreply.github.com>
2025-02-03 15:34:03 +0300

    doing addAdmin page

--- src/pages/AddAdminPage.jsx
+++ src/pages/AddAdminPage.jsx
@@ -1,9 +1,41 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
 
 const AddAdminPage = () => {
+    
+    const [users,setUsers] = useState([]);
+
+    useEffect(() => {
+        axios
+            .get(`${process.env.REACT_APP_API_LINK}/api/user/getUsers`)
+            .then((data) => {
+                setUsers(data);
+            })
+    },[])
+
+    // const assignAdmin = async () => {
+
+    // }
+    
     return (
-        <div>
-            <h2>add admin</h2>
+        <div className='App'>
+            <div className="addAdmin">
+                <div className="addAdmin__title">
+                    <h2>Добавить администратора</h2>
+                </div>
+                <div className="addAdmin__users">
+                    {users.map((user) => (
+                        <div className='addAdmin__users_item' key={user.id}>
+                            <div className="addAdmin__users_item-name">
+                                {user.telegram_id}
+                            </div>
+                            <div className="addAdmin__users_item-role">
+                                {user.role}
+                            </div>
+                        </div>
+                    ))}
+                </div>
+            </div>            
         </div>
     )
 }
