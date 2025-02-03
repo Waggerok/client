1cb113d39a77fdb2c6ae4bf4ce61f89982a53fe9
Waggerok <115732069+Waggerok@users.noreply.github.com>
2025-02-03 13:41:30 +0300

    starting doing admin page

--- src/pages/AdminPage.jsx
+++ src/pages/AdminPage.jsx
@@ -2,9 +2,18 @@ import React from 'react';
 
 const AdminPage = () => {
     return (
-        <>
-            
-        </>
+        <div className='App'>
+            <h2 style={{ marginTop: '10px', textAlign: 'center' }}>Админ панель</h2>
+            <div className="admin">
+                <div className="admin__buttons">
+                    <button style={{ backgroundColor: '#0f5bdd', color: 'white' }}>Добавить устройство</button>
+                    <button style={{ backgroundColor: 'white', color: 'red', border: '1px solid red' }}>Удалить устройство</button>
+                    <button style={{ backgroundColor: '#0f5bdd', color: 'white' }}>Редактировать устройство</button>
+                    <button style={{ backgroundColor: '#0f5bdd', color: 'white' }}>Назначить админа</button>
+                    <button style={{ backgroundColor: 'white', color: 'red', border: '1px solid red' }}>Удалить админа</button>
+                </div>
+            </div>
+        </div>
     );
 };
 
