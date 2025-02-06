633e8f3e1dd03ddbb3843db7c0c5ac5713dcd169
Waggerok <115732069+Waggerok@users.noreply.github.com>
2025-02-06 12:50:16 +0300

    added media

--- src/index.js
+++ src/index.js
@@ -4,5 +4,6 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.css';
 import '../src/css/App.min.css';
+import '../src/css/Media.min.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(<App/>);
